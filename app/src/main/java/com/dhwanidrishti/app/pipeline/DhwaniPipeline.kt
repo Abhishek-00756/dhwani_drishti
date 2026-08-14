c63e500fff24980d49f7413190b2508cdc89ab8d
Abhishek-00756 <birabhi012@gmail.com>
2026-08-14 10:52:21 +0530

    narration_mode

--- app/src/main/java/com/dhwanidrishti/app/pipeline/DhwaniPipeline.kt
+++ app/src/main/java/com/dhwanidrishti/app/pipeline/DhwaniPipeline.kt
@@ -113,7 +113,7 @@ class DhwaniPipeline(
                 sonification.updateFromZones(smoothed)
             }
 
-            if (mode == AppMode.SOUNDSCAPE) {
+            if (mode == AppMode.NARRATED || mode == AppMode.HYBRID) {
                 modeBEngine().process(closeness, frame)
             }
 
